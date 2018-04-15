292efb93827653c2e330851dfb47d7d31e45d3f7
Andrew <andrew.bestbier@gmail.com>
2018-04-15 09:34:45 +0100

    Add final solution for Day 1

--- Day1/day1.hsproj/Main.hs
+++ Day1/day1.hsproj/Main.hs
@@ -1,1 +1,16 @@
-hello = "andrew"
+import Prelude 
+
+integerToDigits :: Integer -> [Int]
+integerToDigits n = map (\x -> read [x] :: Int) (show n)
+
+summer (x:y:[]) 
+  | x == y = x
+  | otherwise = 0
+summer (x:y:zs) 
+  | x == y = x + summer (y:zs)
+  | otherwise = summer (y:zs)
+
+day1 input 
+  | head digits == last digits = head digits + summer digits
+  | otherwise = summer digits
+    where digits = integerToDigits input
