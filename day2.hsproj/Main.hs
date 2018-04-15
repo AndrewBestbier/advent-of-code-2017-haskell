22ae8c1c39480c1995a2afa9e9b27512817e9bf8
Andrew <andrew.bestbier@gmail.com>
2018-04-15 11:26:53 +0100

    Add solution for day 2 question 2

--- day2.hsproj/Main.hs
+++ day2.hsproj/Main.hs
@@ -7,10 +7,14 @@ main
   = do
     { ["--file", fname] <- getArgs
     ; input <- readFile fname
-    ; putStr . show $ question1 input }
+    ; putStr . show $ question2 input }
 
 question1 input = sum $ map minMaxDifference $ map words $ lines input 
 
 minMaxDifference input = maximum inputToInts - minimum inputToInts
   where inputToInts =  map read input :: [Int]
         
+question2 input = sum $ map evenlyDivisibleDifference $ map words $ lines input 
+
+evenlyDivisibleDifference input = [ x `div` y | x <- inputToInts, y <- inputToInts, x /= y, x `mod` y == 0] !! 0
+  where inputToInts =  map read input :: [Int]
