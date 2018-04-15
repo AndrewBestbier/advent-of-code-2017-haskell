920b0a8711ac86f982630ea4580c12296e8d528b
Andrew <andrew.bestbier@gmail.com>
2018-04-15 19:24:27 +0100

    Add day 5 question 2 solution

--- day5.hsproj/Main.hs
+++ day5.hsproj/Main.hs
@@ -9,7 +9,7 @@ main
   = do
     { ["--file", fname] <- getArgs
     ; input <- readFile fname
-    ; putStr . show $ question1 input }
+    ; putStr . show $ question2 input }
     
 question1 input = listToStep $ map stringToInt $ lines input
   where stringToInt input = read input :: Int
@@ -25,3 +25,17 @@ stepper input position step
   
 
 
+question2 input = listToStep2 $ map stringToInt $ lines input
+  where stringToInt input = read input :: Int
+  
+
+listToStep2 input = stepper2 (fromList input) 0 0
+ 
+stepper2 input position step
+  | newPosition > ((Seq.length input) - 1) = step + 1
+  | positionValue >= 3 = stepper2 newInputDecreased newPosition (step + 1)
+  | otherwise = stepper2 newInput newPosition (step + 1)
+      where positionValue = index input position
+            newPosition = position + positionValue
+            newInput = update position (positionValue + 1) input
+            newInputDecreased = update position (positionValue - 1) input
