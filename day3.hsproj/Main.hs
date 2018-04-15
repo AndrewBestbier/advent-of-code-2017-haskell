e2b7ae8c1de2e7667d7942817e20ddf6b078033b
Andrew <andrew.bestbier@gmail.com>
2018-04-15 09:38:14 +0100

    Re-organise file structure to days

--- day3.hsproj/Main.hs
+++ day3.hsproj/Main.hs
@@ -9,9 +9,10 @@ type ComplexPoint = (Point, [Point])
 -- Question 1
 question1 n = (\((x:+y), _) -> abs x + abs y) . last . take n $ iterate step1 ((0:+0), E)
 
+  
 -- Question 2
 question2 n = (\((_, _, x), _) -> x) $ last $ takeWhileInclusive (\((_, _, x), _) -> x < n) $ iterate step2 (((0:+0), E, 1), [])
-
+        
 
 -- Helpers
 
@@ -19,7 +20,7 @@ step1 :: (Position, CompassPoint) -> (Position, CompassPoint)
 step1 ((0:+0), E) = (((1:+0), N))
 step1 (oldPosition, compassPoint) = (move oldPosition compassPoint, maybeRotate oldPosition newPosition compassPoint)
   where newPosition = move oldPosition compassPoint
-
+  
 step2 :: ComplexPoint -> ComplexPoint
 step2 (((0:+0), E, 1), []) = (((1:+0), N, 1), [((0:+0), E, 1)])
 step2 ((oldPosition, compassPoint, value), acc) = ((move oldPosition compassPoint, maybeRotate oldPosition newPosition compassPoint, newValue), newAcc)
@@ -33,9 +34,9 @@ move (x :+ y) compassPoint = case compassPoint of
   E -> ((x + 1) :+ y)
   S -> (x :+ (y - 1))
   W -> ((x - 1) :+ y)
-
+  
 maybeRotate :: Position -> Position -> CompassPoint -> CompassPoint
-maybeRotate (x1 :+ y1) (x2 :+ y2) compassPoint
+maybeRotate (x1 :+ y1) (x2 :+ y2) compassPoint 
   | x1 > 0 && y1 < 0 && x1 ==  abs y1 = turnLeft compassPoint
   | x2 == y2 = turnLeft compassPoint
   | abs x2 == y2 = turnLeft compassPoint
@@ -47,7 +48,7 @@ turnLeft compassPoint = case compassPoint of
   W -> S
   S -> E
   E -> N
-
+        
 findNewValue :: Int -> [Point] -> Position -> Int
 findNewValue oldValue acc newPosition = sum [z | ((x1:+y1), d, z) <- acc, (x2:+y2) <- surroundingPoints, x1 == x2, y1 == y2]
   where surroundingPoints = findSurroundingPoints newPosition
